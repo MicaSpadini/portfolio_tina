3c7339439b3beac2dfb4f5fa8ee4fa31561419c4
MicaSpadini <102698058+MicaSpadini@users.noreply.github.com>
2026-02-23 16:09:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// CARROSSEL
 
+document.querySelectorAll(".carousel").forEach(carousel => {
+  let index = 0;
+  const images = carousel.querySelectorAll("img");
+  const next = carousel.querySelector(".next");
+  const prev = carousel.querySelector(".prev");
+
+  next.addEventListener("click", () => {
+    images[index].classList.remove("active");
+    index = (index + 1) % images.length;
+    images[index].classList.add("active");
+  });
+
+  prev.addEventListener("click", () => {
+    images[index].classList.remove("active");
+    index = (index - 1 + images.length) % images.length;
+    images[index].classList.add("active");
+  });
+
+  // MODAL
+  images.forEach(img => {
+    img.addEventListener("click", () => {
+      document.getElementById("modal").style.display = "block";
+      document.getElementById("modal-img").src = img.src;
+    });
+  });
+});
+
+document.querySelector(".close").onclick = function() {
+  document.getElementById("modal").style.display = "none";
+};
